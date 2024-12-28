54d062b5cc382c10382d976689d32083954db546
Marius <76063193+mogli27@users.noreply.github.com>
2024-12-28 22:58:21 +0200

    update

--- camera.js
+++ camera.js
@@ -1,19 +1,21 @@
 var video = document.getElementById("video");
+video.addEventListener("touchstart", capteaza);
+video.addEventListener("mousedown",capteaza);
 //--------------------------------------
-function on_cam_success(stream){
+function on_cam_success(stream) {
     video.srcObject = stream;
 }
 //--------------------------------------
-function on_cam_error(err){
-    alert("error."+err.message);
+function on_cam_error(err) {
+    alert("error." + err.message);
 }
 //--------------------------------------
-var constraints = {audio: false, video: true};
+var constraints = { audio: false, video: true };
 navigator.mediaDevices.getUserMedia(constraints)
-.then(on_cam_success)
-.catch(on_cam_error);
+    .then(on_cam_success)
+    .catch(on_cam_error);
 //-------------------------------------
-function capteaza(){
+function capteaza() {
     var c = document.getElementById("canvas");
     c.width = video.width;
     c.height = video.height;
